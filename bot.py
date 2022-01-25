fe38bd073b33499112513e26aec11922ea886618
lkl089 <kimjunghyeon5801@gmail.com>
2022-01-26 00:25:44 +0900

    check message author
    for download

--- bot.py
+++ bot.py
@@ -82,43 +82,6 @@ async def on_message(message, maskinfo=[],
     if message.author.bot:  # 채팅을 친 사람이 봇일 경우
         return None  # 반응하지 않고 구문을 종료합니다.
 
-    if message.content == prefix + "명령어":
-        embed = discord.Embed(title="명령어 목록", description="현재 사용할수있는 명령어입니다.", color=0x62c1cc)
-        embed.add_field(name=":point_right:!마스크 '주소'", value="입력한 주소지의 공적마스크 판매처를 알려줍니다.", inline=False)
-        embed.add_field(name=":point_right:!한국", value="한국의 코로나 상황을 알려줍니다.", inline=False)
-        embed.add_field(name=":point_right:!정보출처", value="데이터를 가져오는 사이트를 알려줍니다.", inline=False)
-        embed.add_field(name=":point_right:!홍보자료", value="방역수칙 이미지를 불러옵니다", inline=False)
-        embed.add_field(name=":point_right:!세계", value="주요 확진국가의 코로나 확진자 정보를 알려줍니다.", inline=False)
-        embed.add_field(name=":point_right:!'국가명'", value="각 확진국가들의 확진자 정보를 알려줍니다\n"+
-                        "주요 확진국가들을 검색할수있습니다.(19개국)\n"+
-                        "입력 예시) :point_right:!미국, !영국, !프랑스, !스페인 ....", inline=False)
-        embed.add_field(name=":point_right:!아시아, !유럽, !북아메리카, !남아메리카, !아프리카, !오세아니아", value="각 대륙별 코로나 상황을 알려줍니다.", inline=False)
-        await message.channel.send(embed=embed)
-        # DM으로 메시지를 보냅니다.
-        # await message.author.send("응답")
-
-
-    if message.content == prefix + "목록":
-        embed = discord.Embed(title="명령어 목록", description="명령어 목록입니다.", color=0x62c1cc)
-        embed.add_field(name="!한국", value="한국의 코로나 상황을 알려줍니다.", inline=False)
-        embed.add_field(name="!세계", value="전세계 코로나 확진자 정보를  알려줍니다.", inline=False)
-        embed.add_field(name="!마스크 '주소'", value="입력한 주소지의 공적마스크 판매처를 검색합니다", inline=False)
-        embed.add_field(name="!국가목록", value="[추가예정]각 국가명 입력시 해당 국가의 코로나 상황을 알려줍니다.", inline=False)
-        embed.add_field(name="!아시아, !유럽, !북아메리카, !남아메리카, !아프리카, !오세아니아", value="각 대륙별 코로나 상황을 알려줍니다.", inline=False)
-        # embed.add_field(name="!한국", value="한국의 코로나 상황을 알려줍니다.")
-        #id = str(710687517275586580)
-        #emoji = client.get_emoji(str(id))
-        #emoji = 'U+003AU+0066U+006CU+0061U+0067U+005FU+006BU+0072U+003A'
-        await message.channel.send(embed=embed)
-        await message.add_reaction('a:flag_kr:b57d2718c0f2330c0e06166d4b5fb606')
-
-
-        async def on_reaction_add(reaction, user):
-            if reaction.emoji == ':flag_kr:':
-                message.reaction(':flag_kr:')
-                message.reaction(':globe_with_meridians:')
-
-
     if message.content == prefix + "테스트":
         # DB Access Start
 
@@ -143,7 +106,7 @@ async def on_message(message, maskinfo=[],
         while row:
             # 0=CNT, 1=DeviceType, 2=PC_Status, 3=IP, 4=PC_Time, 5=isReal
             print(row[0], row[1], row[2], row[3], row[4], row[5])
-            cnt = str(row[0])
+            allow_id = str(row[0])
             device = str(row[1])
             status = str(row[2])
             ip = str(row[3])
@@ -157,8 +120,8 @@ async def on_message(message, maskinfo=[],
         # DB Access END!
 
         embed = discord.Embed(title="DB 데이터 테스트", color=0xa83232)
-        embed.add_field(name="CNT",value=cnt,inline=False)
-        embed.add_field(name="Device",value=device,inline=False)
+        embed.add_field(name="CNT", value=allow_id, inline=True)
+        embed.add_field(name="Device",value=device,inline=True)
         embed.add_field(name="Status",value=status,inline=False)
         embed.add_field(name="ip",value=ip,inline=False)
         embed.add_field(name="Time",value=time,inline=False)
@@ -166,5 +129,282 @@ async def on_message(message, maskinfo=[],
 
         await message.channel.send(embed=embed)
 
+    if message.content == prefix + "명령어":
+        # DB Access Start
+
+        # DB 서버 주소
+        server = dba.server
+        # 데이터 베이스 이름
+        database = dba.database
+        # 접속 유저명
+        username = dba.username
+        # 접속 유저 패스워드
+        password = dba.password
+
+        # MSSQL 접속
+        cnxn = pymssql.connect(server, username, password, database)
+        cursor = cnxn.cursor()
+
+        # cursor.execute('SELECT * FROM Test_table;')
+        cursor.execute('SELECT * FROM Test_table ORDER BY CAST(CNT AS int) ASC;')
+
+        row = cursor.fetchone()
+
+        while row:
+            # 0=CNT, 1=DeviceType, 2=PC_Status, 3=IP, 4=PC_Time, 5=isReal
+            print(row[0], row[1], row[2], row[3], row[4], row[5])
+            allow_id = str(row[0])
+            device = str(row[1])
+            status = str(row[2])
+            ip = str(row[3])
+            time = str(row[4])
+            real = str(row[5])
+            row = cursor.fetchone()
+
+        print('Read DB!!')
+        cnxn.close()
+
+        # DB Access END!
+
+        embed = discord.Embed(title="VRCHAT 명령어", color=0xa83232)
+        embed.add_field(name="!아바타",value='아바타 목록을 표시합니다.',inline=False)
+        embed.add_field(name="!옷",value='아바타별 옷 목록을 표시합니다.',inline=False)
+        embed.add_field(name="!악세사리",value='악세사리 목록을 표시합니다.',inline=False)
+        embed.add_field(name="!SDK",value='VRCSDK 버전을 표시합니다.',inline=False)
+
+        await message.channel.send(embed=embed)
+
+    if message.content == prefix + "아바타":
+        # DB Access Start
+
+        # DB 서버 주소
+        server = dba.server
+        # 데이터 베이스 이름
+        database = dba.database
+        # 접속 유저명
+        username = dba.username
+        # 접속 유저 패스워드
+        password = dba.password
+
+        # MSSQL 접속
+        cnxn = pymssql.connect(server, username, password, database)
+        cursor = cnxn.cursor()
+
+        # cursor.execute('SELECT * FROM Test_table;')
+        cursor.execute('SELECT * FROM Test_table ORDER BY CAST(CNT AS int) ASC;')
+
+        row = cursor.fetchone()
+
+        while row:
+            # 0=CNT, 1=DeviceType, 2=PC_Status, 3=IP, 4=PC_Time, 5=isReal
+            print(row[0], row[1], row[2], row[3], row[4], row[5])
+            allow_id = str(row[0])
+            device = str(row[1])
+            status = str(row[2])
+            ip = str(row[3])
+            time = str(row[4])
+            real = str(row[5])
+            row = cursor.fetchone()
+
+        print('Read DB!!')
+        cnxn.close()
+
+        # DB Access END!
+
+        embed = discord.Embed(title="VRCHAT 명령어", color=0xa83232)
+        embed.add_field(name="!아바타",value='아바타 목록을 표시합니다.',inline=False)
+        embed.add_field(name="!옷",value='아바타별 옷 목록을 표시합니다.',inline=False)
+        embed.add_field(name="!악세사리",value='악세사리 목록을 표시합니다.',inline=False)
+        embed.add_field(name="!SDK",value='VRCSDK 버전을 표시합니다.',inline=False)
+
+        await message.channel.send(embed=embed)
+
+    if message.content == prefix + "옷":
+        # DB Access Start
+
+        # DB 서버 주소
+        server = dba.server
+        # 데이터 베이스 이름
+        database = dba.database
+        # 접속 유저명
+        username = dba.username
+        # 접속 유저 패스워드
+        password = dba.password
+
+        # MSSQL 접속
+        cnxn = pymssql.connect(server, username, password, database)
+        cursor = cnxn.cursor()
+
+        # cursor.execute('SELECT * FROM Test_table;')
+        cursor.execute('SELECT * FROM Test_table ORDER BY CAST(CNT AS int) ASC;')
+
+        row = cursor.fetchone()
+
+        while row:
+            # 0=CNT, 1=DeviceType, 2=PC_Status, 3=IP, 4=PC_Time, 5=isReal
+            print(row[0], row[1], row[2], row[3], row[4], row[5])
+            allow_id = str(row[0])
+            device = str(row[1])
+            status = str(row[2])
+            ip = str(row[3])
+            time = str(row[4])
+            real = str(row[5])
+            row = cursor.fetchone()
+
+        print('Read DB!!')
+        cnxn.close()
+
+        # DB Access END!
+
+        embed = discord.Embed(title="VRCHAT 명령어", color=0xa83232)
+        embed.add_field(name="!아바타",value='아바타 목록을 표시합니다.',inline=False)
+        embed.add_field(name="!옷",value='아바타별 옷 목록을 표시합니다.',inline=False)
+        embed.add_field(name="!악세사리",value='악세사리 목록을 표시합니다.',inline=False)
+        embed.add_field(name="!SDK",value='VRCSDK 버전을 표시합니다.',inline=False)
+
+        await message.channel.send(embed=embed)
+
+
+    if message.content == prefix + "악세사리":
+        # DB Access Start
+
+        # DB 서버 주소
+        server = dba.server
+        # 데이터 베이스 이름
+        database = dba.database
+        # 접속 유저명
+        username = dba.username
+        # 접속 유저 패스워드
+        password = dba.password
+
+        # MSSQL 접속
+        cnxn = pymssql.connect(server, username, password, database)
+        cursor = cnxn.cursor()
+
+        # cursor.execute('SELECT * FROM Test_table;')
+        cursor.execute('SELECT * FROM Test_table ORDER BY CAST(CNT AS int) ASC;')
+
+        row = cursor.fetchone()
+
+        while row:
+            # 0=CNT, 1=DeviceType, 2=PC_Status, 3=IP, 4=PC_Time, 5=isReal
+            print(row[0], row[1], row[2], row[3], row[4], row[5])
+            allow_id = str(row[0])
+            device = str(row[1])
+            status = str(row[2])
+            ip = str(row[3])
+            time = str(row[4])
+            real = str(row[5])
+            row = cursor.fetchone()
+
+        print('Read DB!!')
+        cnxn.close()
+
+        # DB Access END!
+
+        embed = discord.Embed(title="VRCHAT 명령어", color=0xa83232)
+        embed.add_field(name="!아바타",value='아바타 목록을 표시합니다.',inline=False)
+        embed.add_field(name="!옷",value='아바타별 옷 목록을 표시합니다.',inline=False)
+        embed.add_field(name="!악세사리",value='악세사리 목록을 표시합니다.',inline=False)
+        embed.add_field(name="!SDK",value='VRCSDK 버전을 표시합니다.',inline=False)
+
+        await message.channel.send(embed=embed)
+
+    if message.content == prefix + "SDK":
+        # DB Access Start
+
+        # DB 서버 주소
+        server = dba.server
+        # 데이터 베이스 이름
+        database = dba.database
+        # 접속 유저명
+        username = dba.username
+        # 접속 유저 패스워드
+        password = dba.password
+
+        # MSSQL 접속
+        cnxn = pymssql.connect(server, username, password, database)
+        cursor = cnxn.cursor()
+
+        # cursor.execute('SELECT * FROM Test_table;')
+        cursor.execute('SELECT * FROM Test_table ORDER BY CAST(CNT AS int) ASC;')
+
+        row = cursor.fetchone()
+
+        while row:
+            # 0=CNT, 1=DeviceType, 2=PC_Status, 3=IP, 4=PC_Time, 5=isReal
+            print(row[0], row[1], row[2], row[3], row[4], row[5])
+            allow_id = str(row[0])
+            device = str(row[1])
+            status = str(row[2])
+            ip = str(row[3])
+            time = str(row[4])
+            real = str(row[5])
+            row = cursor.fetchone()
+
+        print('Read DB!!')
+        cnxn.close()
+
+        # DB Access END!
+
+        embed = discord.Embed(title="VRCHAT 명령어", color=0xa83232)
+        embed.add_field(name="!아바타",value='아바타 목록을 표시합니다.',inline=False)
+        embed.add_field(name="!옷",value='아바타별 옷 목록을 표시합니다.',inline=False)
+        embed.add_field(name="!악세사리",value='악세사리 목록을 표시합니다.',inline=False)
+        embed.add_field(name="!SDK",value='VRCSDK 버전을 표시합니다.',inline=False)
+
+        await message.channel.send(embed=embed)
+
+    if message.content == prefix + "다운로드":
+        # DB Access Start
+
+        # DB 서버 주소
+        server = dba.server
+        # 데이터 베이스 이름
+        database = dba.database
+        # 접속 유저명
+        username = dba.username
+        # 접속 유저 패스워드
+        password = dba.password
+
+        # MSSQL 접속
+        cnxn = pymssql.connect(server, username, password, database)
+        cursor = cnxn.cursor()
+
+        # cursor.execute('SELECT * FROM Test_table;')
+        cursor.execute('SELECT Access_ID FROM [VRCHAT].[dbo].[access];')
+
+        row = cursor.fetchone()
+
+        while row:
+            # 0=CNT, 1=DeviceType, 2=PC_Status, 3=IP, 4=PC_Time, 5=isReal
+            print(row[0])
+            allow_id = str(row[0])
+            #device = str(row[1])
+            #status = str(row[2])
+            #ip = str(row[3])
+            #time = str(row[4])
+            #real = str(row[5])
+            row = cursor.fetchone()
+        print(allow_id)
+        print('Read DB!!')
+        cnxn.close()
+
+        # DB Access END!
+
+        your_id = message.author
+        print(your_id)
+        print(allow_id)
+
+        if str(your_id) == str(allow_id):
+            embed = discord.Embed(title="다운로드 링크", color=0xa83232)
+            embed.add_field(name="아바타", value='아바타 정보', inline=False)
+            embed.add_field(name="다운로드 링크", value='링크 테스트', inline=False)
+
+            await message.channel.send(embed=embed)
+        else:
+            embed = discord.Embed(title=":x:권한 없음!:x:",description="권한이 없는 디스코드 계정입니다.", color=0xCD1039)
+
+            await message.channel.send(embed=embed)
 
 client.run(token1)
